50cd3d26221945e923faa205b8313c2cae6b31c1
Yash Sharma <146658822+yashrookie@users.noreply.github.com>
2024-11-19 22:48:49 +0530

    pdi by recursion

---
+++ patterns/arrays/recursion/pdi.java
@@ -0,0 +1,19 @@
+package recursion;
+import java.util.Scanner;
+public class pdi {
+    public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pdi(n);
+    }
+    public static void pdi(int n ){
+        if(n == 0){
+            return ;
+        }
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+}
+    
+
