bba1601ed943ceee06310b9f4626b2702872c70a
Yash Sharma <146658822+yashrookie@users.noreply.github.com>
2024-11-26 23:17:36 +0530

    last index using recursion in array

---
+++ patterns/arrays/recursion/lastIndex.java
@@ -0,0 +1,32 @@
+package recursion;
+import java.util.Scanner;
+public class lastIndex {
+    public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i]= scn.nextInt();
+        }
+        int x = scn.nextInt();
+        int li = lastIndex(arr,0,x);
+        System.out.println(li);
+    }
+    public static int lastIndex(int[]arr,int idx,int x){
+        if(idx == arr.length){
+            return -1;
+        }
+        int liisa = lastIndex(arr,idx+1,x);
+        if(liisa == -1){
+            if(arr[idx] == x){
+                return idx;
+            }
+            else {
+                return -1;
+            }
+        }
+        else {
+            return liisa;
+        }
+    }
+}
