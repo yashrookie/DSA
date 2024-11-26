7bfcfc29c44f4f64872b618474b200f4f1fa6f3e
Yash Sharma <146658822+yashrookie@users.noreply.github.com>
2024-11-26 19:01:53 +0530

    display arrays using recursion

---
+++ patterns/arrays/recursion/displayArr.java
@@ -0,0 +1,20 @@
+package recursion;
+import java.util.Scanner;
+public class displayArr {
+    public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i]= scn.nextInt();
+        }
+        displayArr(arr,0);
+    }
+    public static void displayArr(int []arr,int idx){
+        if(idx == arr.length){
+            return;
+        }
+        System.out.println(arr[idx]);
+        displayArr(arr,idx+1);
+    }
+}
